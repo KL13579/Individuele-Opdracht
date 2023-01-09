95bb580516718cffe25f6a38dd1809b06015684a
KL13579 <114230460+KL13579@users.noreply.github.com>
2023-01-09 11:16:22 +0100

    Update Airport_Traffic.py

--- Airport_Traffic.py
+++ Airport_Traffic.py
@@ -377,9 +377,9 @@ with tab2:
 #Code voor de lineplots
     else:
         lineplot = px.line(Totaal_per_land, x = Totaal_per_land.index, y = land_variabele, 
-                      title = "Totaal ATM's per Land per Jaar '" + land_variabele + "'",
-                     labels = {Totaal_per_land.index : "Tijd (Jaren)",
-                              land_variabele : "Aantal ATM's in '" + land_variabele + "'"})
+                      title = "Totaal ATM's per Land per Jaar '" + land_variabele + "'")
+        lineplot.update_xaxes(title = "Tijd (Jaren)")
+        lineplot.update_yaxes(title = "Aantal ATM's")
         st.plotly_chart(lineplot)
 
 
